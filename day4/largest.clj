c5517a51e253d347d778c49107e64133575ff42d
Laura Dean <lgdean@users.noreply.github.com>
2015-08-26 23:58:16 -0700

    working but crummy

--- day4/largest.clj
+++ day4/largest.clj
@@ -1,13 +1,31 @@
 (ns largest
   (:require [clojure.test :refer :all]))
 
+(defn do-insert
+  [x sorted-vals]
+  (cond (empty? sorted-vals) (list x)
+        (< x (first sorted-vals)) (cons x sorted-vals)
+        :else (cons (first sorted-vals) (do-insert x (rest sorted-vals)))))
+
+(defn maybe-insert
+  [x sorted-vals]
+  (if (<= x (first sorted-vals)) sorted-vals
+      (do-insert x (rest sorted-vals))))
+
 (defn nth-largest
   [n vals]
-  22)
+  (let [init-largest (sort (take n vals))
+        helper (fn helper [so-far others]
+                 (cond (empty? others) (first so-far)
+                       :else (helper (maybe-insert (first others) so-far)
+                                     (rest others))))]
+    (helper init-largest (drop n vals))))
 
 (deftest nth-largest-test
   (testing "should find largest"
     (is (= 5 (nth-largest 1 [3,5,1]))))
+  (testing "nice short example"
+    (is (= 5 (nth-largest 1 [5]))))
   (testing "should handle example given"
     (is (= 5 (nth-largest 2 [5,2,4,7,1])))))
 
