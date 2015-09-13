7715c9eedf1caada0095d79aa119d5e14e6f9ae8
Laura Dean <lgdean@users.noreply.github.com>
2015-09-12 23:06:03 -0400

    tests for the anagrams problem, in case i want to implement, yay

---
+++ day4/anagrams.clj
@@ -0,0 +1,20 @@
+(ns anagrams
+  (:require [clojure.test :refer :all]))
+
+;; wonder whether i'll ever implement this.
+
+(defn anagrams?
+  [one other]
+  true)
+
+(deftest anagrams-test
+  (testing "empty strings, yes"
+    (is (true? (anagrams? "" ""))))
+  (testing "should handle example given"
+    (is (true? (anagrams? "silent" "listen"))))
+  (testing "but not other non-anagram things"  
+    (is (false? (anagrams? "silent" "listennn")))
+    (is (false? (anagrams? "silent" "silens"))))
+)
+
+(run-tests)
